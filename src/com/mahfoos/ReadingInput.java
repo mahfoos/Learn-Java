9ab3bc8fb91bc3b498bbe6b809f2a42ad38b2505
Mahfooz <umahfoosahamed@gmail.com>
2021-05-29 07:41:52 +0530

    added input

--- src/com/mahfoos/ReadingInput.java
+++ src/com/mahfoos/ReadingInput.java
@@ -1,8 +1,19 @@
 package com.mahfoos;
 
+import java.util.Scanner;
+
 public class ReadingInput {
 
     public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in); // System in mean reading from terminal
+        System.out.print("Enter your age");
+        byte age = sc.nextByte();
+        System.out.print("Name : ");
+        String name = sc.next().trim(); // next read only one token instead we use nextLine
+        System.out.println("My Name " + name);
+        System.out.println("Your age" + age);
+
 
     }
 }
---
+++ src/com/mahfoos/Variable.java
@@ -0,0 +1,9 @@
+package com.mahfoos;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        int age = 10;  // int is type of variable and age is identifier
+        System.out.println(age);
+    }
+}
