f2c3261e7de954152f0105729deb9f035d421aa0
Mahfooz <umahfoosahamed@gmail.com>
2021-05-30 08:57:46 +0530

    completed basic

---
+++ src/com/mahfoos/basics/ForEachLoop.java
@@ -0,0 +1,16 @@
+package com.mahfoos.basics;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String [] fruits = {"Apple", "Mango", "Orange"};
+
+        for(int i = 0; i < fruits.length; i++){
+            System.out.println(fruits[i]);
+        }
+
+        // for each loop
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
--- src/com/mahfoos/basics/ForLoops.java
+++ src/com/mahfoos/basics/ForLoops.java
@@ -1,10 +1,26 @@
 package com.mahfoos.basics;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ForLoops {
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++){
-            System.out.println("Hello world");
+            System.out.println("Hello world " + i);
+        }
+
+        int x = 0;
+        while (x > 0){
+            System.out.println("Hello world " + x);
+            x++;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")){
+            System.out.println("Input: ");
+            input = sc.next().toLowerCase();
         }
 
     }
